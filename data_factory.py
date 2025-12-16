4e9a77accaef17c8190831b1a68cd4b51954399e
박기준 <gj.park@okestro.com>
2025-12-16 09:46:51 +0900

    [FIX] Fix dataloader module for completion of implementation

--- data_factory.py
+++ data_factory.py
@@ -19,3 +19,30 @@ class AD_Dataset(Dataset):
         y = self.labelset[idx]
 
         return X, y
+
+
+def data_provider(args, flag):
+    dataset = AD_Dataset(args.dataset_name, args.dataset_dir)
+
+    train_size = math.floor(0.8 * len(dataset))
+    test_size = len(dataset) - train_size
+
+    if flag == 'val':
+        shuffle_flag = True
+        drop_last = True
+        batch_size = args.batch_size
+        _, dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+    else:
+        shuffle_flag = True
+        drop_last = True
+        batch_size = args.batch_size
+        dataset, _ = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+    data_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle_flag,
+        num_workers=args.num_workers,
+        drop_last=drop_last
+    )
+    return data_loader
